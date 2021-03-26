1465f27ad272952e49ab316274726d23b20dd3e5
Phuoc Nguyen <me@phuoc.ng>
2021-03-26 16:17:20 +0700

    Move products to sidebar

---
+++ _data/products.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        name: 'Blur Page',
+        url: 'https://blur.page',
+        description: 'A browser extension to hide sensitive information on a web page',
+    },
+    {
+        name: 'Form Validation',
+        url: 'https://formvalidation.io',
+        description: 'The best validation library for JavaScript',
+    },
+    {
+        name: 'React PDF Viewer',
+        url: 'https://react-pdf-viewer.dev',
+        description: 'A React component to view a PDF document',
+    },
+    {
+        name: '1 LOC',
+        url: 'https://1loc.dev',
+        description: 'Favorite JavaScript utilities in single line of code',
+    },
+    {
+        name: 'CSS Layout',
+        url: 'https://csslayout.io',
+        description: 'A collection of popular layouts and patterns made with CSS',
+    },
+    {
+        name: 'this VS that',
+        url: 'https://thisthat.dev',
+        description: 'The differences between _ and _ in the front-end development',
+    },
+];
